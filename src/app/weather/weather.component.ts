c7ac1926ab2b5586c4643f039f69a76348916d5c
KathrinShpigel <kathrin.shpigel.90@gmail.com>
2020-11-15 00:18:46 +0300

    feat: change city for get weather

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -11,16 +11,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class WeatherComponent implements OnInit {
 
-  city = 'Краснодар';
+  city = localStorage.getItem('city2$') || 'Краснодар';
   data: any;
   weather: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http
-      .get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&lang=ru&appid=8a73b01baf0566caac65bbce78f63431&units=metric`)
-      .subscribe((data: any) => this.getWeather(data));
+    this.getData();
   }
 
   getWeather(data: any): {} {
@@ -29,6 +27,46 @@ export class WeatherComponent implements OnInit {
       class: `owf-${data.weather[0].id}`,
       descr: data.weather[0].description,
     };
+  }
+
+  getData(e?): void {
+    this.http
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&lang=ru&appid=8a73b01baf0566caac65bbce78f63431&units=metric`)
+      .subscribe(
+        (data: any) => this.getWeather(data),
+        (error: any) => {
+          e.target.textContent = this.city = 'Краснодар';
+          localStorage.removeItem('city2$');
+          this.setCity('err');
+        }
+      );
+  }
+
+  setCity(e?): void {
+    if (e.type === 'keypress') {
+      if (e.which === 13 || e.keyCode === 13) {
+        if (e.target.textContent === '') {
+          e.target.textContent = this.city = localStorage.getItem('city2$') || 'Краснодар';
+          return;
+        }
+        localStorage.setItem('city2$', e.target.textContent);
+        this.city = e.target.textContent;
+      }
+    } else if (e === 'err') {
+      this.getData();
+    } else {
+      if (e.target.textContent === '') {
+        e.target.textContent = this.city = localStorage.getItem('city2$') || 'Краснодар';
+        return;
+      }
+      localStorage.setItem('city2$', e.target.textContent);
+      this.city = e.target.textContent;
+      this.getData(e);
+    }
+  }
+
+  clearCity(e): void {
+    e.target.textContent = '';
   }
 
 }
