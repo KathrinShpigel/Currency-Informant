11cfc44ac1a6356b774daf650945523ae1089dde
KathrinShpigel <kathrin.shpigel.90@gmail.com>
2020-12-14 23:32:28 +0300

    feat: add sort data of table

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
 
 import { CurrencyInformantService } from '../currency-informant.service';
 
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,9 +1,19 @@
 import { Component, AfterViewInit, ViewChild, OnInit } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { CurrencyInformantService } from '../currency-informant.service';
 
+export interface PeriodicElement {
+  Cur_Abbreviation: string;
+  Cur_Scale: number;
+  curName: number;
+  todayCurRate: number;
+  yesterdayCurRate: number;
+  change: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [];
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -15,19 +25,34 @@ export class TableComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = [
     'Cur_Abbreviation', 'Cur_Scale', 'curName', 'todayCurRate', 'yesterdayCurRate', 'change'
   ];
-  dataSource = new MatTableDataSource();
 
-  @ViewChild(MatSort) sort: MatSort;
+  dataSource;
+
+  //@ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
 
   constructor(ci: CurrencyInformantService) {
     this.ci = ci;
   }
 
   ngOnInit(): void {
+
+
   }
 
   ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
+    if (!this.ci.loading) {
+      this.ci.currenciesListLast2Days.forEach(el => {
+        ELEMENT_DATA.push(el)
+      });
+      this.dataSource = new MatTableDataSource(ELEMENT_DATA);
+      this.dataSource.sort = this.sort;
+      const sortState: Sort = {active: 'name', direction: 'desc'};
+      this.sort.active = sortState.active;
+      this.sort.direction = sortState.direction;
+      this.sort.sortChange.emit(sortState);
+      console.log(this.sort);
+    }
   }
 
 }
